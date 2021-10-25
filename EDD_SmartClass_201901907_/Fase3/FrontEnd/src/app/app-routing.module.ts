a78065619ef3dc3b3f6c7aed86693de2072eb189
Yasmin0210 <69336924+Byron-Estuardo@users.noreply.github.com>
2021-10-25 17:47:15 -0600

    Pagina Principal

--- EDD_SmartClass_201901907_/Fase3/FrontEnd/src/app/app-routing.module.ts
+++ EDD_SmartClass_201901907_/Fase3/FrontEnd/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { PaginaPrincipalComponent } from './Vistas/pagina-principal/pagina-principal.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistroComponent } from './Vistas/registro/registro.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: PaginaPrincipalComponent
+  },
   {
     path: '/Registro',
     component: RegistroComponent
--- EDD_SmartClass_201901907_/Fase3/FrontEnd/src/app/app.module.ts
+++ EDD_SmartClass_201901907_/Fase3/FrontEnd/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistroComponent } from './Vistas/registro/registro.component';
+import { PaginaPrincipalComponent } from './Vistas/pagina-principal/pagina-principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistroComponent
+    RegistroComponent,
+    PaginaPrincipalComponent
   ],
   imports: [
     BrowserModule,
